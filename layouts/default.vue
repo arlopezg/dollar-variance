80011537c4452cef0ccde8300faad9fc3d16e5c1
Alejandro López <alejandrolopez.gd@gmail.com>
2020-08-08 02:08:56 -0400

    feat: initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-screen flex flex-col justify-between">
+    <p>ei</p>
+    <Nuxt class="container" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 960px;
+  margin: 20px auto;
+  padding: 0 15px;
+  @screen md {
+    padding: 0;
+  }
+}
+</style>
