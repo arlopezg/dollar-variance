6274d53435f3c95f2b24194a4eb5b9625cb59ad5
Alejandro López <alejandrolopez.gd@gmail.com>
2020-08-08 22:43:24 -0400

    feat: agrega funcionalidad a store de divisas

--- store/currency.js
+++ store/currency.js
@@ -1,7 +1,35 @@
-export const state = () => ({})
+import CurrencyService from '~/services/CurrencyService';
 
-export const getters = {}
+export const state = () => ({
+  currencies: null,
+  currency: null
+});
 
-export const mutations = {}
+export const getters = {};
 
-export const actions = {}
+export const mutations = {
+  SET_ALL_CURRENCIES(state, payload) {
+    state.currencies = payload;
+  },
+  SET_CURRENCY_VALUES(state, payload) {
+    state.currency = payload;
+  }
+};
+
+export const actions = {
+  async getCurrencyValues({ commit }, currency = 'dolar') {
+    const response = await CurrencyService.getCurrencyValuesFromLastMonth(
+      currency
+    );
+
+    commit('SET_CURRENCY_VALUES', response.data);
+  },
+  async getAllCurrencies({ commit }) {
+    const response = await CurrencyService.getCurrenciesState();
+    const currencies = Object.values(response);
+    const filteredCurrencies = currencies.filter(
+      (currency) => typeof currency === 'object'
+    );
+    commit('SET_ALL_CURRENCIES', filteredCurrencies);
+  }
+};
