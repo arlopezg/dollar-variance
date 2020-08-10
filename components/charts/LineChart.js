db3393beaaae922311c2ccf12c890a30e185e024
Alejandro López <alejandrolopez.gd@gmail.com>
2020-08-10 04:04:44 -0400

    feat: mejora rendimiento en LineChart

--- components/charts/LineChart.js
+++ components/charts/LineChart.js
@@ -1,5 +1,7 @@
 import { Line } from 'vue-chartjs';
 
+import numberFormat from '~/utils/formatting/number-formatter';
+
 export default {
   extends: Line,
   props: {
@@ -9,8 +11,41 @@ export default {
       validator(data = {}) {
         return data.labels && data.datasets;
       }
-    },
-    options: { type: Object, required: false }
+    }
+  },
+  data() {
+    return {
+      options: {
+        animation: { duration: 0 },
+        hover: { animationDuration: 0 },
+        responsiveAnimationDuration: 0,
+        responsive: true,
+        scales: {
+          xAxes: [
+            {
+              gridLines: { display: false },
+              ticks: { maxTicksLimit: 7 }
+            }
+          ],
+          yAxes: [
+            {
+              ticks: {
+                callback(label) {
+                  return numberFormat(label, {
+                    style: 'currency',
+                    currency: 'CLP'
+                  });
+                }
+              }
+            }
+          ]
+        },
+        lineStyling: {
+          lineTension: 2,
+          backgroundColor: 'red'
+        }
+      }
+    };
   },
   mounted() {
     this.renderChart(this.data, this.options);
