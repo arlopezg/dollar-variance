959105998cb62cb9210fa3214e3c03a789943ea9
Alejandro López <alejandrolopez.gd@gmail.com>
2020-08-10 05:51:58 -0400

    fix: corrige zIndex de footer

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <footer class="w-full bg-blue-900 p-1 text-gray-300 text-center">
+  <footer
+    class="relative z-15 w-full bg-blue-900 p-1 text-gray-300 text-center"
+  >
     <p>Alejandro López. {{ currentYear }}</p>
   </footer>
 </template>
